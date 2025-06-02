3ea062a8d4d42235bdcab928d3e965ad20b9e0c4
ghali <ghali@DESKTOP-VTPLRO2>
2025-06-02 21:49:20 +0100

    Add project files.

---
+++ NeuraSpace/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace NeuraSpace
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Black;
+            label1.Font = new Font("Segoe UI Emoji", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.SpringGreen;
+            label1.Location = new Point(171, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(216, 47);
+            label1.TabIndex = 0;
+            label1.Text = "NueraSpace";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.MenuText;
+            textBox1.ForeColor = Color.AntiqueWhite;
+            textBox1.Location = new Point(169, 228);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(235, 43);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.WindowText;
+            label2.Font = new Font("Segoe UI Emoji", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.SpringGreen;
+            label2.Location = new Point(169, 187);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 26);
+            label2.TabIndex = 2;
+            label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.WindowText;
+            label3.Font = new Font("Segoe UI Emoji", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.SpringGreen;
+            label3.Location = new Point(169, 303);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 26);
+            label3.TabIndex = 4;
+            label3.Text = "Password";
+            label3.Click += label3_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ActiveCaptionText;
+            textBox2.Location = new Point(169, 344);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(235, 43);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.LimeGreen;
+            label4.Font = new Font("Segoe UI Emoji", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonFace;
+            label4.Location = new Point(220, 513);
+            label4.Name = "label4";
+            label4.Padding = new Padding(40, 15, 40, 15);
+            label4.Size = new Size(144, 56);
+            label4.TabIndex = 5;
+            label4.Text = "Login";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.LimeGreen;
+            label5.Font = new Font("Segoe UI Emoji", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonFace;
+            label5.Location = new Point(169, 588);
+            label5.Name = "label5";
+            label5.Padding = new Padding(40, 15, 40, 15);
+            label5.Size = new Size(235, 56);
+            label5.TabIndex = 6;
+            label5.Text = "Create Account";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI Emoji", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox1.ForeColor = Color.SpringGreen;
+            checkBox1.Location = new Point(171, 420);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(113, 30);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Remeber\r\n";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = SystemColors.InfoText;
+            ClientSize = new Size(611, 803);
+            Controls.Add(checkBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ButtonFace;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            ImeMode = ImeMode.On;
+            Name = "Form1";
+            Text = "Login Page";
+            TransparencyKey = Color.Transparent;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label5;
+        private CheckBox checkBox1;
+
+        // Add this method to the Form1 class
+        private void label3_Click(object sender, EventArgs e)
+        {
+            // Add any desired functionality here, or leave it empty if no action is needed
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Draw curved background for label4 ("Login")
+            DrawRoundedRectangle(e.Graphics, label4.Bounds, 30, Color.LimeGreen);
+
+            // Draw curved background for label5 ("Create Account")
+            DrawRoundedRectangle(e.Graphics, label5.Bounds, 30, Color.LimeGreen);
+        }
+
+        // Helper method to draw a rounded rectangle
+        private void DrawRoundedRectangle(Graphics g, Rectangle bounds, int radius, Color fillColor)
+        {
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddArc(bounds.X, bounds.Y, radius, radius, 180, 90);
+                path.AddArc(bounds.Right - radius, bounds.Y, radius, radius, 270, 90);
+                path.AddArc(bounds.Right - radius, bounds.Bottom - radius, radius, radius, 0, 90);
+                path.AddArc(bounds.X, bounds.Bottom - radius, radius, radius, 90, 90);
+                path.CloseFigure();
+
+                using (SolidBrush brush = new SolidBrush(fillColor))
+                {
+                    g.FillPath(brush, path);
+                }
+            }
+        }
+    }
+}
---
+++ NeuraSpace/Form1.cs
@@ -0,0 +1,35 @@
+namespace NeuraSpace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void title_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
